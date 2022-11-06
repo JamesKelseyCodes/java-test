4398cdbbe1195cfd5b678bbfa5338d1ae5e13d13
JamesKelseyCodes <james.edward.kelsey@gmail.com>
2022-11-06 15:18:47 +1030

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+        System.out.println("Welcome to count down!");
+        System.out.println("Please choose a number to count down from: ");
+        String name = "James";
+        Scanner myInt = new Scanner(System.in); // creating a scanner object
+        int x = myInt.nextInt(); // read user input
+        System.out.println("Hello " + name + ", the initial number is: " + x);
+        double d = Math.random();
+        //this is a test comment
+        while (x > 1) {
+            System.out.println("the number is now: " + x);
+            if (x % 10 == 0) {
+                System.out.println("Wow, " + x + " is a milestone!");
+            }
+                    x = x - 1;
+                    Thread.sleep(200);
+        }
+        System.out.println("Well, that's the end of the list!");
+        }
+    }
+
